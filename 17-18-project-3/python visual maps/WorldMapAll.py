e89ebb83849b91d87e7f3a52a2d61bf2f8c63e32
Francisco Saavedra <saavedra.francisco@outlook.com>
2023-10-16 19:08:00 -0500

    adding python visual maps

---
+++ 17-18-project-3/python visual maps/WorldMapAll.py
@@ -0,0 +1,32 @@
+#import required libraries
+import os
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+plt.style.use('fivethirtyeight')
+import plotly.express as px
+
+#read the csv file
+file_path = os.path.join(os.getcwd(), 'data.csv')
+df = pd.read_csv(file_path)
+print(df.head())
+
+df = pd.DataFrame(df)
+
+# Group by nationality and count occurrences
+nationality_counts = df['Nationality'].value_counts().reset_index()
+nationality_counts.columns = ['Nationality', 'Occurrences']
+
+# Use plotly.express to create a choropleth map
+fig = px.choropleth(
+    nationality_counts,
+    locations="Nationality",
+    locationmode='country names',
+    color='Occurrences',
+    hover_name="Nationality",
+    title='Number of Occurrences of Nationality on Map',
+    projection='natural earth'
+)
+
+fig.show()
